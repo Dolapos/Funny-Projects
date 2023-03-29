c6fae7e44a71956cb9c9a81086bc3b23554147cf
Dolapos <111833888+Dolapos@users.noreply.github.com>
2023-03-28 21:58:10 -0500

    Update guessing_game.py

--- guessing_game.py
+++ guessing_game.py
@@ -1,10 +1,14 @@
 #Guessing Game
 import random 
-num = random.randint(1,100)
+num = random.randint(1,1000)
 
 guess = 0
 
-pick = int(input('What is your guess? [choose between 1 and 100] '))
+print(f'A number has been generated.')
+print(f'Take a guess!')
+print(sep = '\n')
+
+pick = int(input('What is your guess? [choose between 1 and 1000] '))
 guess += 1
 while (pick != num):
     guess += 1
