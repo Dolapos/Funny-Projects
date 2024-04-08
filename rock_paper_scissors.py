ef7cfde0247a862b0a7a03a32a60981ee86953e9
dolapos <111833888+Dolapos@users.noreply.github.com>
2024-04-08 00:32:29 -0500

    Update rock_paper_scissors.py

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -1,17 +1,28 @@
 import random
 import time
 
+
 class RPS:
-    def __init__(self, player = None, computer = None):
+    def __init__(self, player=None, computer=None):
         self.choices = ['rock', 'paper', 'scissors']
         self.player = player
         self.computer = computer
+        self.player_wins = 0
+        self.computer_wins = 0
+        self.total_games = 0
 
     def __str__(self):
+        print()
         return f'Player: {self.player} | Computer: {self.computer}'
 
     def player_input(self):
         while True:
+            print('Welcome to Rock, Paper, Scissors!')
+            time.sleep(2)
+            print('This is a game of pure luck!')
+            time.sleep(2)
+            print('You will be playing against a computer. You play on \'SHOOT!\'')
+            time.sleep(2)
             print('Rock, Paper, Scissors...')
             time.sleep(2)
             player_choice = input('SHOOT!: ').lower()
@@ -25,13 +36,48 @@ class RPS:
         self.computer = random.choice(self.choices)
 
     def play_game(self):
-        if (self.computer == 'rock' and self.player == 'scissors') or (self.computer == 'paper' and self.player == 'rock') or (self.computer == 'scissors' and self.player == 'paper'):
+        if ((self.computer == 'rock' and self.player == 'scissors') or
+                (self.computer == 'paper' and self.player == 'rock') or
+                (self.computer == 'scissors' and self.player == 'paper')):
+            self.computer_wins += 1
+            self.total_games += 1
             return 'Computer is the winner!'
         elif self.computer == self.player:
             time.sleep(2)
             return f'Draw!'
         else:
+            self.player_wins += 1
+            self.total_games += 1
             return f'Player is the winner!'
+
+    def counter(self):
+        while True:
+            print()
+            print('Do you want to play again?')
+            player_choice1 = input('Yes or No? ').lower()
+            print()
+            if player_choice1 == 'yes':
+                self.repeat()
+                self.comp_input()
+                result = self.play_game()
+                print(self.__str__())
+                print(result)
+            else:
+                print('Thanks for playing! \n')
+                print(f'Player Wins: {self.player_wins}')
+                print(f'Computer Wins: {self.computer_wins} \n')
+                print(f'Total Games: {self.total_games}')
+                break
+
+    def repeat(self):
+        print('Rock, Paper, Scissors...')
+        time.sleep(2)
+        player_choice2 = input('SHOOT!: ').lower()
+        if player_choice2 in self.choices:
+            self.player = player_choice2
+        else:
+            print('Please provide a valid input.')
+
 
 def main():
     play = RPS()
@@ -39,6 +85,8 @@ def main():
     play.comp_input()
     print(play.__str__())
     print(play.play_game())
+    play.counter()
+
 
 if __name__ == '__main__':
     main()
